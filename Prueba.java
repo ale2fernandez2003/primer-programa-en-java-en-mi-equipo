d3f69c58374e6616ffc41a94c06c1d14500cb821
Alejandro <ale2fernandez2003@gmail.com>
2023-09-29 13:21:13 +0200

    añadi mi edad y la fecha del nacimiento

--- Prueba.java
+++ Prueba.java
@@ -3,6 +3,7 @@ public class Prueba{
 	public static void main(String args[]){
 
 		System.out.println("Fernández Fernández");
+		System.out.println("Tengo 20 años y naci el 14 de mayo del 2003");
 	}
 
 
