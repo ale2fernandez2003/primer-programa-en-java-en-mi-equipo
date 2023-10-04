14fa5f7e25d1ddeea208ded54a56621290bae8bf
Alejandro <ale2fernandez2003@gmail.com>
2023-10-04 13:08:25 +0200

    primeros cambios

--- Prueba.java
+++ Prueba.java
@@ -2,9 +2,9 @@ public class Prueba{
 
 	public static void main(String args[]){
 
-		System.out.println("Fernández Fernández");
 		System.out.println("Tengo 20 años y naci el 14 de mayo del 2003");
 		System.out.println("Las hamburguesas");
+		System.out.println("Kidd Keo");
 	}
 
 
